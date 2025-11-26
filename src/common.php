7a599e7059b3b66e91219f16b649b0a1a4952fd7
gabbo <gabri152007@gmail.com>
2025-11-26 11:38:25 +0100

    uso di include_once

---
+++ src/common.php
@@ -0,0 +1,20 @@
+<?php
+$servername = 'db';
+$username = 'myuser';
+$password = 'mypassword';
+$database = 'myapp_db';
+
+echo $servername . "<br />";
+echo $username . "<br />";
+echo $password . "<br />";
+echo $database . "<br />";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if($conn->connect_error){
+    die("Connesione fallita :" . $conn->connect_error);
+}
+
+echo "<h1>Connesione riuscita a MySQL!</h1>";
+
+?>
--- src/index.php
+++ src/index.php
@@ -1,21 +1,5 @@
 <?php
-$servername = 'db';
-$username = 'myuser';
-$password = 'mypassword';
-$database = 'myapp_db';
-
-echo $servername . "<br />";
-echo $username . "<br />";
-echo $password . "<br />";
-echo $database . "<br />";
-
-$conn = new mysqli($servername, $username, $password, $database);
-
-if($conn->connect_error){
-    die("Connesione fallita :" . $conn->connect_error);
-}
-
-echo "<h1>Connesione riuscita a MySQL!</h1>";
+include_once('common.php');
 
 $result = $conn->query("Show TABLES;");
 echo "<pre>";
